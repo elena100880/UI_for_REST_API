504559978b727a699c11530437794f71a986b68b
elena100880 <100880kr@gmail.com>
2021-06-28 00:42:17 +0200

    add Product Delete method and template for Product Edit template method

--- src/Controller/ProductInStoreController.php
+++ src/Controller/ProductInStoreController.php
@@ -38,8 +38,7 @@ class ProductInStoreController extends AbstractController
                                                                         'choices' => [  'all items' => 1,
                                                                                         'zero amount items' => 0,
                                                                                         'more than five items' => 5],
-                                                                        'data' => 1,
-                                                                                    ])
+                                                                        'data' => 1])
                                     ->add('send', SubmitType::class, ['label'=>'Show chosen'])
                                     ->getForm();
         $form->handleRequest($request);
@@ -48,16 +47,16 @@ class ProductInStoreController extends AbstractController
         {
             $dataFromForm = $form->getData();
             $itemsToSearch = $dataFromForm['items'];
+
+            $query = ['amount' => $itemsToSearch];
          
-            if ($itemsToSearch == 0) $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products?amount=0' );
-            if ($itemsToSearch == 5) $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products?amount=5' );
-            if ($itemsToSearch == 1) $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products?amount=1' );
+            if ($itemsToSearch == 0) $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products', ['query' => $query ]);
+            if ($itemsToSearch == 5) $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products', ['query' => $query ]);
+            if ($itemsToSearch == 1) $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products', ['query' => $query ]);
         }
         else $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products' );
         
-        $jsonDataFromAPI = $response->getContent(false);
-        $arrayDataFromAPI = json_decode($jsonDataFromAPI, true); 
-
+        $arrayDataFromAPI = $this->array_data_from_response($response);
         $developerMessage = $this->get_dev_info($arrayDataFromAPI, $response);
        
         if ($response->getStatusCode() == 200) {
@@ -82,15 +81,13 @@ class ProductInStoreController extends AbstractController
     public function product_add(Request $request): Response
     {
         $form = $this->createFormBuilder()           /** @todo make ProductInStore object TypeForm */
-                                    ->add('name', TextType::class, ['label' => 'Name of Product:',
-                                                                    'required' => false])
-                                    ->add('amount', TextType::class, ['label' => 'Amount in Store:',
-                                                                        'data' => 0])
-                                    ->add('save', SubmitType::class, ['label'=>'Add the item'])
+                                    ->add('name', TextType::class, ['label' => 'Name of Product:'])
+                                    ->add('amount', NumberType::class, ['label' => 'Amount in Store:', 'data' => 0, 'required' => false ])
+                                    ->add('save', SubmitType::class, ['label'=>'Add Product'])
                                     ->getForm();
         $form->handleRequest($request);
     
-        $developerMessage = null;
+        $developerMessage = "Not connected to API yet";
         $userMessage = null;
         if ($form->isSubmitted()) {
 
@@ -105,71 +102,122 @@ class ProductInStoreController extends AbstractController
 
             $jsonToAddProduct = json_encode(['name' => $name, 'amount' => $amount]);  /** @todo encode from object ProductInStore */
             $response = $this->client->request( 'POST',  'http://'.ProductInStoreController::IP.'/products', ['body' => $jsonToAddProduct]);
-            $jsonDataFromAPI = $response->getContent(false);
-            $arrayDataFromAPI = json_decode($jsonDataFromAPI, true);
 
+            $arrayDataFromAPI = $this->array_data_from_response($response);
             $developerMessage = $this->get_dev_info($arrayDataFromAPI, $response);
             
             if ($response->getStatusCode() == 200) {
-                $this->addFlash('succes','Product was seccesfully added!!');
+                $this->addFlash('message','Product was seccesfully added!!');
                 return $this->redirectToRoute('products_all');
             }
             else $userMessage = "DB is not available. Please, try again later.";
         }
-        
-                
-            $contents = $this->renderView('product_in_store/product_add.html.twig', [
-                'ip' => ProductInStoreController::IP,
-                'form' => $form->createView(),
-                'developerMessage' => $developerMessage,
-                'userMessage' => $userMessage,
-            ]);
-            return new Response($contents);
-        
-    }
-
-    #[Route('/{id}', name: 'product_in_store_show', methods: ['GET'])]
-    public function show(ProductInStore $productInStore): Response
-    {
-        return $this->render('product_in_store/show.html.twig', [
-            'product_in_store' => $productInStore,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'product_in_store_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, ProductInStore $productInStore): Response
-    {
-        $form = $this->createForm(ProductInStoreType::class, $productInStore);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('product_in_store_index');
-        }
-
-        return $this->render('product_in_store/edit.html.twig', [
-            'product_in_store' => $productInStore,
+                        
+        $contents = $this->renderView('product_in_store/product_add.html.twig', [
             'form' => $form->createView(),
+            'developerMessage' => $developerMessage,
+            'userMessage' => $userMessage,
         ]);
+        return new Response($contents);
     }
 
-    #[Route('/{id}', name: 'product_in_store_delete', methods: ['POST'])]
-    public function delete(Request $request, ProductInStore $productInStore): Response
+    public function product_edit(Request $request, $id): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$productInStore->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($productInStore);
-            $entityManager->flush();
+        $response = $this->client->request( 'GET', 'http://'.ProductInStoreController::IP.'/products/'.$id);
+
+        $arrayDataFromAPI = $this->array_data_from_response($response);
+        $developerMessage = $this->get_dev_info($arrayDataFromAPI, $response);
+
+        if ($response->getStatusCode() == 200) {
+            $product = $arrayDataFromAPI['data'];
+        }
+        //preventing open edit-page if Product was deleted
+        elseif ($response->getStatusCode() == 404) {
+            $this->addFlash('message','Product was already deleted!!');
+            return $this->redirectToRoute('products_all');
+        }
+        else {
+            $this->addFlash('message','Editing is not available. Please, try again later!!');
+            return $this->redirectToRoute('products_all');
         }
 
-        return $this->redirectToRoute('product_in_store_index');
+        $form = $this->createFormBuilder()           /** @todo make ProductInStore object TypeForm */
+                                    ->add('name', TextType::class, ['label' => 'Name of Product:', 'data' => $product['name'], 'required' => false ])
+                                    ->add('amount', NumberType::class, ['label' => 'Amount in Store:', 'data' => $product['amount'], 'required' => false ])
+                                    ->add('save', SubmitType::class, ['label'=>'Save changes'])
+                                    ->add('delete', SubmitType::class, ['label'=>'Delete Product!!'])
+                                    ->getForm();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted()) {
+            if ($form->getClickedButton()->getName() == 'delete') return $this->redirectToRoute('product_delete', ['id' => $id]);
+            else {
+
+                $dataFromForm = $form->getData();
+                $name = $dataFromForm['name'];               
+                $amount = intval($dataFromForm['amount']);
+                
+                $userMessage = null;
+                /** 
+                 * @todo some validation of user's input data: name and amount 
+                 * if not Valid return some $userMessage
+                 */
+
+                $jsonToAddProduct = json_encode(['name' => $name, 'amount' => $amount]);  /** @todo encode from object ProductInStore */
+                $response = $this->client->request( 'PATCH',  'http://'.ProductInStoreController::IP.'/products', ['body' => $jsonToAddProduct]);
+
+                $arrayDataFromAPI = $this->array_data_from_response($response);
+                $developerMessage = $this->get_dev_info($arrayDataFromAPI, $response);
+                
+                if ($response->getStatusCode() == 200) {
+                    $this->addFlash('message','Product was seccesfully updated!!');
+                    return $this->redirectToRoute('product_edit');
+                }
+                else $userMessage = "DB is not available. Please, try again later.";
+            }
+        }
+        
+        $contents = $this->renderView('product_in_store/product_edit.html.twig', [
+            'form' => $form->createView(),
+            'developerMessage' => $developerMessage,
+            'userMessage' => $userMessage,
+        ]);
+        return new Response($contents);
+        
     }
 
-    public function get_dev_info($arrayDataFromAPI, $response)
+    public function product_delete(Request $request, $id)
     {
-        $message = $arrayDataFromAPI['message'] ?? "-";
+        $response = $this->client->request( 'DELETE', 'http://'.ProductInStoreController::IP.'/products/'.$id);
+
+     //   $jsonDataFromAPI = $response->getContent(false); //а что я получу, если вернет не мой обработанный ответ, а симфони ошибку выдаст свою? или //ПарамКонвертер свое 500 вернет, когда БД недоступна??? тогда httpClient должен их обработать и выдать мне сюда 500
+      //  $arrayDataFromAPI = json_decode($jsonDataFromAPI, true);
+
+        $arrayDataFromAPI = $this->array_data_from_response($response);
+                
+        if ($response->getStatusCode() == 200) {
+            $this->addFlash('message','Product was seccesfully deleted!!');
+            return $this->redirectToRoute('products_all');
+        }
+        else {
+            $this->addFlash('message','Deleting is not available at the moment. Please, try again later!!');
+            return $this->redirectToRoute('product_edit', ['id' => $id]);
+        }
+
+        $this->addFlash('message','Product was seccesfully added!!');
+        return $this->redirectToRoute('products_all');
+    }
+
+    private function get_dev_info($arrayDataFromAPI, $response)
+    {
+        $message = $arrayDataFromAPI['message'] ?? "--";
         $code = $response->getStatusCode();
-        return $developerMessage = "message: $message, code: $code";
+        $devInfo = $arrayDataFromAPI['devInfo'] ?? "--";
+        return "message: $message, code: $code, devInfo: $devInfo";
+    }
+
+    private function array_data_from_response($response) {
+        $jsonDataFromAPI = $response->getContent(false);
+        return json_decode($jsonDataFromAPI, true);
     }
 }
